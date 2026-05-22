f7c65bf5977f9bf313e20bf5fed62aab3ea7d184
wendellgust <wendell_gustavo001@hotmail.com>
2026-05-22 14:17:34 +0100

    Adding more scenary

---
+++ victim-c/entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+mkdir -p /run/sshd /var/log /var/log/lab
+ssh-keygen -A 2>/dev/null
+cat > /etc/ssh/sshd_config << 'CONF'
+Port 22
+PermitRootLogin yes
+PasswordAuthentication yes
+UsePAM no
+ChallengeResponseAuthentication no
+KbdInteractiveAuthentication no
+PrintMotd no
+LogLevel VERBOSE
+AuthorizedKeysFile .ssh/authorized_keys
+MaxStartups 50
+CONF
+touch /var/log/auth.log
+exec /usr/sbin/sshd -D -e 2>> /var/log/auth.log
